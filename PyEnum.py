8ccb5f45f2fbb6578d298ab01bed2e3e3d92d007
sanggon6107 <sanggon6107@naver.com>
2023-01-04 22:09:09 +0900

    Refactor : EnumFromZero와 TESTER 클래스 분리
    Enum의 기능적인 부분과 Enum의 요소들을 다른 클래스로 분리

--- PyEnum.py
+++ PyEnum.py
@@ -1,7 +1,7 @@
 from enum import auto, IntEnum
 
 
-class TESTER(IntEnum) :
+class EnumFromZero(IntEnum) :
     def _generate_next_value_(name, start, count, last_values) :
         return count
 
@@ -11,9 +11,14 @@ class TESTER(IntEnum) :
     def __repr__(self) :
         return self.name
 
+class TESTER(EnumFromZero) :
     TESTER1 = auto()
     TESTER2 = auto()
     TESTER3 = auto()
-    
+
+
 if __name__ == '__main__' :
-    print(TESTER.TESTER1 < TESTER.TESTER2)
+    print([str(i) for i in TESTER])
+    print(TESTER.TESTER1 == 0)
+    print(TESTER.TESTER2 == 1)
+    print(TESTER.TESTER3 == 2)
