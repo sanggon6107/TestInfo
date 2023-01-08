12a9f3c7dcac68c9790db2fc6390f51cd9019097
sanggon6107 <sanggon6107@naver.com>
2023-01-08 16:30:29 +0900

    Feat : Singleton implemented
    TestInfo는 반드시 하나만 생성되어야 하므로 싱글톤 구현

--- TestInfo.py
+++ TestInfo.py
@@ -8,8 +8,20 @@ class TestInfo :
     site   : list[str] = field(default_factory=list)
 
     def __post_init__(self) -> None :
-        self.tester = ['' for _ in range(len(TESTER))]
-        self.site   = ['' for _ in range(len(TESTER))]
+        cls = type(self)
+        if not hasattr(cls, "_post_init") :
+            
+            self.tester = ['' for _ in range(len(TESTER))]
+            self.site   = ['' for _ in range(len(TESTER))]
+            
+            cls._post_init = True
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
 
 if __name__ == '__main__' :
     test_info = TestInfo()
+    test_info2 = TestInfo()
