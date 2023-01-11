69c58ecc1399c19dae3ed1de7d996a8361bf3659
sanggon6107 <sanggon6107@naver.com>
2023-01-11 22:24:06 +0900

    Feat : Implement __post_init__ __post_init__에서 테스트 SW의 구성을 초기화 하도록 구현

--- TestInfo.py
+++ TestInfo.py
@@ -4,16 +4,32 @@ from dataclasses import dataclass, field
 
 @dataclass
 class TestInfo :
-    tester : list[str] = field(default_factory=list)
-    site   : list[str] = field(default_factory=list)
+    num_site : list[int] = field(default_factory=list[int])
+    pc       : list = field(default_factory=list)
+    site     : list = field(default_factory=list)
+    
 
     def __post_init__(self) -> None :
         cls = type(self)
         if not hasattr(cls, "_post_init") :
             
-            self.tester = ['' for _ in range(len(TESTER))]
-            self.site   = ['' for _ in range(len(TESTER))]
-            
+            for test in TESTER :
+                match (test) :
+                    case TESTER.TESTER1 :
+                        self.num_site.append(4)
+                        self.pc.append(['TESTER1_1','TESTER1_2'])
+                        self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4'])
+                    
+                    case TESTER.TESTER2 :
+                        self.num_site.append(5)
+                        self.pc.append(['TESTER2_1','TESTER2_2', 'TESTER2_3'])
+                        self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4', 'SITE5'])
+
+                    case TESTER.TESTER3 :
+                        self.num_site.append(8)
+                        self.pc.append(['TESTER3_1','TESTER3_2', 'TESTER3_3', 'TESTER3_4'])
+                        self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4', 'SITE5', 'SITE6', 'SITE7', 'SITE8'])
+
             cls._post_init = True
 
     def __new__(cls, *args, **kwargs):
