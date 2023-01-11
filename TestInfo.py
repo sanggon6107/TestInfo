81c2be1ee9ca9645b13346d06f09ba09a4aa3803
sanggon6107 <sanggon6107@naver.com>
2023-01-11 22:59:12 +0900

    Feat : Add CheckTestInfoLen 각 데이터들의 길이가 다르다면, 초기화가 잘못된 것이므로 도중에 멈추도록 구현

--- TestInfo.py
+++ TestInfo.py
@@ -2,17 +2,16 @@ from PyEnum import *
 from dataclasses import dataclass, field
 
 
-@dataclass
+@dataclass(frozen=True)
 class TestInfo :
     num_site : list[int] = field(default_factory=list[int])
-    pc       : list = field(default_factory=list)
-    site     : list = field(default_factory=list)
-    
+    pc       : list      = field(default_factory=list)
+    site     : list      = field(default_factory=list)
+
 
     def __post_init__(self) -> None :
         cls = type(self)
         if not hasattr(cls, "_post_init") :
-            
             for test in TESTER :
                 match (test) :
                     case TESTER.TESTER1 :
@@ -29,15 +28,25 @@ class TestInfo :
                         self.num_site.append(8)
                         self.pc.append(['TESTER3_1','TESTER3_2', 'TESTER3_3', 'TESTER3_4'])
                         self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4', 'SITE5', 'SITE6', 'SITE7', 'SITE8'])
+                
+                if self.__CheckTestInfoLen() == False :
+                    print("Failed to initialize TestInfo.")
+                    return 
 
             cls._post_init = True
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args, **kwargs) :
         if not hasattr(cls, "_instance"):
             cls._instance = super().__new__(cls)
         return cls._instance
 
+    def __CheckTestInfoLen(self) -> bool :
+        if not (len(self.num_site) == len(self.pc) == len(self.site)) :
+            return False
+        return True
 
 if __name__ == '__main__' :
     test_info = TestInfo()
-    test_info2 = TestInfo()
+    print(test_info.num_site)
+    test_info.num_site[TESTER.TESTER1]=100
+    print(test_info.num_site)
