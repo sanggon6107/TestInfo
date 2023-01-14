3547fea48752a55b991710e7ff68fb153709c4df
sanggon6107 <sanggon6107@naver.com>
2023-01-14 16:44:41 +0900

    Refactor : re-deploy/re-name __CheckDataLen __CheckDataLen으로 함수 이름 변경 빌드를 위한 데이터가 이상하게 작성되었을 때 프로그램 강제 종료

--- TestInfo.py
+++ TestInfo.py
@@ -1,5 +1,6 @@
 from PyEnum import *
 from dataclasses import dataclass, field
+import sys
 
 
 @dataclass(frozen=True)
@@ -7,6 +8,7 @@ class TestInfo :
     num_site : list[int] = field(default_factory=list[int])
     pc       : list      = field(default_factory=list)
     site     : list      = field(default_factory=list)
+    pgm      : list      = field(default_factory=list)
 
 
     def __post_init__(self) -> None :
@@ -18,20 +20,22 @@ class TestInfo :
                         self.num_site.append(4)
                         self.pc.append(['TESTER1_1','TESTER1_2'])
                         self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4'])
+                        self.pgm.append(['PGM1_1', 'PGM1_2', 'PGM1_3', 'PGM1_4'])
                     
                     case TESTER.TESTER2 :
                         self.num_site.append(5)
                         self.pc.append(['TESTER2_1','TESTER2_2', 'TESTER2_3'])
                         self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4', 'SITE5'])
+                        self.pgm.append(['PGM2_1', 'PGM2_2', 'PGM2_3', 'PGM2_4', 'PGM2_5'])
 
                     case TESTER.TESTER3 :
                         self.num_site.append(8)
                         self.pc.append(['TESTER3_1','TESTER3_2', 'TESTER3_3', 'TESTER3_4'])
                         self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4', 'SITE5', 'SITE6', 'SITE7', 'SITE8'])
-                
-                if self.__CheckTestInfoLen() == False :
-                    print("Failed to initialize TestInfo.")
-                    return 
+                        self.pgm.append(['PGM3_1', 'PGM3_2', 'PGM3_3', 'PGM3_4', 'PGM3_5', 'PGM3_6', 'PGM3_7', 'PGM3_8'])
+
+            if not self.__CheckDataLen() :
+                sys.exit("Failed to initialize TestInfo.")
 
             cls._post_init = True
 
@@ -40,8 +44,8 @@ class TestInfo :
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __CheckTestInfoLen(self) -> bool :
-        if not (len(self.num_site) == len(self.pc) == len(self.site)) :
+    def __CheckDataLen(self) -> bool :
+        if not (len(self.num_site) == len(self.pc) == len(self.site) == len(self.pgm)) :
             return False
         return True
 
