8cee18689942d028154f7153b066129446f59461
sanggon6107 <sanggon6107@naver.com>
2023-01-15 16:06:02 +0900

    Style : re-deploy if post_init의 if문을 재배치하여 불필요한 들여쓰기 낮춤 test용 함수 삭제

--- TestInfo.py
+++ TestInfo.py
@@ -14,36 +14,34 @@ class TestInfo :
 
     def __post_init__(self) -> None :
         cls = type(self)
-        if not hasattr(cls, "_post_init") :
-            for tester in TESTER :
-                match (tester) :
-                    case TESTER.TESTER1 :
-                        self.num_site.append(4)
-                        self.pc.append(['TESTER1_1','TESTER1_2'])
-                        self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4'])
-                        self.pgm.append(['PGM1_1', 'PGM1_2', 'PGM1_3', 'PGM1_4'])
-                        self.additional_cal_site.append(self.__SetAdditionalCalSite(tester, 'None'))
+        if hasattr(cls, "_post_init") : TestInfo.__ReturnSingletonError()
 
-                    case TESTER.TESTER2 :
-                        self.num_site.append(5)
-                        self.pc.append(['TESTER2_1','TESTER2_2', 'TESTER2_3'])
-                        self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4', 'SITE5'])
-                        self.pgm.append(['PGM2_1', 'PGM2_2', 'PGM2_3', 'PGM2_4', 'PGM2_5'])
-                        self.additional_cal_site.append(self.__SetAdditionalCalSite(tester, 'All'))
+        for tester in TESTER :
+            match (tester) :
+                case TESTER.TESTER1 :
+                    self.num_site.append(4)
+                    self.pc.append(['TESTER1_1','TESTER1_2'])
+                    self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4'])
+                    self.pgm.append(['PGM1_1', 'PGM1_2', 'PGM1_3', 'PGM1_4'])
+                    self.additional_cal_site.append(self.__SetAdditionalCalSite(tester, 'None'))
 
-                    case TESTER.TESTER3 :
-                        self.num_site.append(8)
-                        self.pc.append(['TESTER3_1','TESTER3_2', 'TESTER3_3', 'TESTER3_4'])
-                        self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4', 'SITE5', 'SITE6', 'SITE7', 'SITE8'])
-                        self.pgm.append(['PGM3_1', 'PGM3_2', 'PGM3_3', 'PGM3_4', 'PGM3_5', 'PGM3_6', 'PGM3_7', 'PGM3_8'])
-                        self.additional_cal_site.append(self.__SetAdditionalCalSite(tester, 0, 1, 5))
+                case TESTER.TESTER2 :
+                    self.num_site.append(5)
+                    self.pc.append(['TESTER2_1','TESTER2_2', 'TESTER2_3'])
+                    self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4', 'SITE5'])
+                    self.pgm.append(['PGM2_1', 'PGM2_2', 'PGM2_3', 'PGM2_4', 'PGM2_5'])
+                    self.additional_cal_site.append(self.__SetAdditionalCalSite(tester, 'All'))
 
-            if not self.__CheckDataLen() :
-                self.__ReturnDataLenError()
+                case TESTER.TESTER3 :
+                    self.num_site.append(8)
+                    self.pc.append(['TESTER3_1','TESTER3_2', 'TESTER3_3', 'TESTER3_4'])
+                    self.site.append(['SITE1', 'SITE2', 'SITE3', 'SITE4', 'SITE5', 'SITE6', 'SITE7', 'SITE8'])
+                    self.pgm.append(['PGM3_1', 'PGM3_2', 'PGM3_3', 'PGM3_4', 'PGM3_5', 'PGM3_6', 'PGM3_7', 'PGM3_8'])
+                    self.additional_cal_site.append(self.__SetAdditionalCalSite(tester, 0, 1, 5))
 
-            cls._post_init = True
+        if not self.__CheckDataLen() : self.__ReturnDataLenError()
 
-        else : TestInfo.__ReturnSingletonError()
+        cls._post_init = True
 
     def __new__(cls, *args, **kwargs) :
         if not hasattr(cls, "_instance"):
@@ -86,12 +84,9 @@ class TestInfo :
         traceback.print_stack(limit=4)
         return sys.exit("Failed to initialize Testinfo. : TestInfo re-instantiated")
 
-    def test(self) :
-        print()
 
 if __name__ == '__main__' :
     test_info = TestInfo()
-    test_info.test()
     print(test_info.num_site)
     test_info.num_site[TESTER.TESTER1]=100
     print(test_info.num_site)
